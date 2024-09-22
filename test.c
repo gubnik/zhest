b2202d283fa41fe154f982825ee61352f65cb84f
gubnik <danger8217.nik@gmail.com>
2024-09-22 15:22:22 +0300

    Changed include paths

--- test.c
+++ test.c
@@ -1,10 +1,17 @@
-#include "headers/list.h"
+#include "incl/list.h"
+#include "incl/syscalls.h"
 #include <stdio.h>
 
 int main (int argc, char ** argv)
 {
-  llnode * node = init_node();
-  printf("%lu | %p\n", sizeof(llnode), node);
+  llnode * node = init_node(42);
+  if (!node)
+  {
+    printf("Node : %p\n", node);
+    sysexit(0);
+  }
+  printf("Node : %p %p %lu\n", node, node->next, node->data);
   destroy_node(node);
+  sysexit(42);
   return 0;
 }
