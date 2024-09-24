68843dc63f86f4d1011ba46054ed4bcb3a0c3e27
gubnik <danger8217.nik@gmail.com>
2024-09-25 00:51:05 +0300

    Updated test

--- test.c
+++ test.c
@@ -16,11 +16,19 @@ int main (int argc, char ** argv)
   llnode * root = 0;
   charout('a');
   charout('\n');
-  //testdptr(&root);
-  //printf("%p\n", root);
   printf("PtrPtr : %p\nPtr: %p\n", &root, root);
   handleargs(&root, 9, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L);
   iterate_list(root, print_node);
+
+  llnode * iter = root->next;
+  stddata sum = 0;
+  while (iter)
+  {
+    sum += iter->data;
+    iter = iter->next;
+  }
+  printf("Sum : %lu\n", sum);
+
   sysexit(42);
   
   return 0;
